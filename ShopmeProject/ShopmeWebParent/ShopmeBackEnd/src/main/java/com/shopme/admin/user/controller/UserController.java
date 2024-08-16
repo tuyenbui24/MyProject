432e54289d07fa3f5f8f96d8741293a24cf48107
Bui Quang Tuyen <quangtuyen10x@gmail.com>
2024-08-16 08:25:36 +0700

    update

---
+++ ShopmeProject/ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/user/controller/UserController.java
@@ -0,0 +1,183 @@
+package com.shopme.admin.user.controller;
+
+import com.shopme.admin.FileUploadUntil;
+import com.shopme.admin.user.UserNotFoundExp;
+import com.shopme.admin.user.UserService;
+import com.shopme.admin.user.export.UserCsvExporter;
+import com.shopme.admin.user.export.UserExcelExporter;
+import com.shopme.admin.user.export.UserPdfExporter;
+import com.shopme.common.entity.Role;
+import com.shopme.common.entity.User;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.util.StringUtils;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import java.io.IOException;
+import java.util.List;
+
+@Controller
+public class UserController {
+
+    @Autowired
+    private UserService userService;
+
+    @GetMapping("/Users")
+    public String ListFirstPage(Model model) {
+        return ListAllUsersByPage(1, model, "firstName", "asc", "");
+    }
+//        return ListAllUsersByPage(1, model);
+
+    @GetMapping("/Users/page/{pageNum}")
+    public String ListAllUsersByPage(@PathVariable(name = "pageNum") int pageNum, Model model,
+                                     @RequestParam(value = "sortField", defaultValue = "firstName") String sortField,
+                                     @RequestParam(value = "sortDirection", defaultValue = "asc") String sortDir,
+                                     @RequestParam(value = "keyword", defaultValue = "") String keyword) {
+
+        Page<User> page = userService.listByPage(pageNum, sortField, sortDir, keyword);
+        List<User> listUsers = page.getContent();
+
+        System.out.println(sortField);
+        System.out.println(sortDir);
+
+        long startCount = (pageNum - 1) * userService.USERS_PER_PAGE + 1;
+        long endCount = startCount + userService.USERS_PER_PAGE - 1;
+        if (endCount > page.getTotalElements()) {
+            endCount = page.getTotalElements();
+        }
+
+        String reverseSortDir = sortDir.equals("asc") ? "desc" : "asc";
+
+        System.out.println("pageNum = " + pageNum);
+        System.out.println("totalPages = " + page.getTotalPages());
+        System.out.println("totalElements = " + page.getTotalElements());
+
+        model.addAttribute("currentPage", pageNum);
+        model.addAttribute("startCount", startCount);
+        model.addAttribute("endCount", endCount);
+        model.addAttribute("totalItem", page.getTotalElements());
+        model.addAttribute("listUsers", listUsers);
+        model.addAttribute("sortField", sortField);
+        model.addAttribute("sortDirection", sortDir);
+        model.addAttribute("reverseSortDir", reverseSortDir);
+        model.addAttribute("totalPages", page.getTotalPages());
+        model.addAttribute("keyword", keyword);
+
+        return "users";
+    }
+
+    // form user
+    @GetMapping("/users/new")
+    public String newUser(Model model) {
+        List<Role> listRoles = userService.listAllRoles();
+
+        User user = new User();
+        user.setEnabled(true);
+
+        model.addAttribute("user", user);
+        model.addAttribute("listRoles", listRoles);
+        model.addAttribute("pageTitle", "Create New User");
+
+        return "user_form";
+    }
+
+    // create user
+    @PostMapping("/users/save")
+    public String saveUser(User user, RedirectAttributes redirectAttributes,
+                           @RequestParam("image") MultipartFile multipartFile, Model model) throws IOException {
+        if (!multipartFile.isEmpty()) {
+            String fileName = StringUtils.cleanPath(multipartFile.getOriginalFilename());
+            user.setPhotos(fileName);
+            User savedUser = userService.save(user);
+
+            String uploadDir = "user-photos/" + savedUser.getId();
+
+            FileUploadUntil.cleanDir(uploadDir);
+            FileUploadUntil.saveFile(uploadDir, fileName, multipartFile);
+//            AmazonS3Util.removeFolder(uploadDir);
+//            AmazonS3Util.uploadFile(uploadDir, fileName, multipartFile.getInputStream());
+        } else {
+            if (user.getPhotos().isEmpty()) user.setPhotos(null);
+            userService.save(user);
+        }
+        redirectAttributes.addFlashAttribute("message", "The user has been saved successfully.");
+        return "redirect:/Users";
+//        return getRedirectURLtoAffectedUser(Users);
+    }
+
+    // edit user
+    @GetMapping("/users/edit/{id}")
+    public String editUser(@PathVariable(name = "id") Integer id, Model model, RedirectAttributes redirectAttributes) {
+        try {
+            User user = userService.get(id);
+            List<Role> listRoles = userService.listAllRoles();
+
+            model.addAttribute("user", user);
+            model.addAttribute("pageTitle", "Edit User (ID: " + id + ")");
+            model.addAttribute("listRoles", listRoles);
+
+            return "user_form";
+        } catch (UserNotFoundExp e) {
+            redirectAttributes.addFlashAttribute("message", e.getMessage());
+            return "redirect:/Users";
+        }
+    }
+
+    // delete user
+    @GetMapping("/users/delete/{id}")
+    public String deleteUser(@PathVariable(name = "id") Integer id,
+                             Model model,
+                             RedirectAttributes redirectAttributes) {
+        try {
+            userService.delete(id);
+            redirectAttributes.addFlashAttribute("message",
+                    "The user ID " + id + " has been deleted successfully");
+        } catch (UserNotFoundExp ex) {
+            redirectAttributes.addFlashAttribute("message", ex.getMessage());
+        }
+        return "redirect:/Users";
+    }
+
+    // edit status
+    @GetMapping("/users/{id}/enabled/{status}")
+    public String updateUserEnabledStatus(@PathVariable("id") Integer id, @PathVariable("status") boolean enabled,
+                                          RedirectAttributes redirectAttributes) throws UserNotFoundExp {
+        userService.updateUserEnabledStatus(id, enabled);
+        String status = enabled ? "enabled" : "disabled";
+        String message = "The user ID " + id + " has been " + status;
+        redirectAttributes.addFlashAttribute("message", message);
+
+        return "redirect:/Users";
+    }
+
+    // dow CSV, EXCEL, PDF
+    @GetMapping("/users/export/csv")
+    public void exportToCSV(HttpServletResponse response) throws IOException {
+        List<User> listUsers = userService.listAllUsers();
+        UserCsvExporter exporter = new UserCsvExporter();
+        exporter.export(listUsers, response);
+    }
+
+    @GetMapping("/users/export/excel")
+    public void exportToExcel(HttpServletResponse response) throws IOException {
+        List<User> listUsers = userService.listAllUsers();
+        UserExcelExporter exporter = new UserExcelExporter();
+        exporter.export(listUsers, response);
+    }
+
+    @GetMapping("/users/export/pdf")
+    public void exportToPDF(HttpServletResponse response) throws IOException {
+        List<User> listUsers = userService.listAllUsers();
+        UserPdfExporter exporter = new UserPdfExporter();
+        exporter.export(listUsers, response);
+    }
+
+}
--- ShopmeProject/ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/user/controller/UserResController.java
+++ ShopmeProject/ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/user/controller/UserResController.java
@@ -1,5 +1,6 @@
-package com.shopme.admin.user;
+package com.shopme.admin.user.controller;
 
+import com.shopme.admin.user.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
